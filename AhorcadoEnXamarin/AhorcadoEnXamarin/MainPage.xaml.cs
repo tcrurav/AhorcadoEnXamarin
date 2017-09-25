5921cb74bf34b03dbefeaae04cd183fb89779e46
tcrurav <tiburcio.cruz@gmail.com>
2017-09-25 17:12:55 +0100

    al finalizar la clase de 20170925

--- AhorcadoEnXamarin/AhorcadoEnXamarin/MainPage.xaml.cs
+++ AhorcadoEnXamarin/AhorcadoEnXamarin/MainPage.xaml.cs
@@ -9,9 +9,25 @@ namespace AhorcadoEnXamarin
 {
     public partial class MainPage : ContentPage
     {
+        readonly string[] ALFABETO = { "A", "B", "C", "D", "E", "F", "G"};
         public MainPage()
         {
             InitializeComponent();
+
+            Inicializar();
+        }
+
+        private void Inicializar()
+        {
+            imageAhorcado.Source = ImageSource.FromResource("AhorcadoEnXamarin.img.1.png");
+            int fila = 0;
+            for(int columna = 0; columna < 7; columna++)
+            {
+                Button boton = new Button();
+                boton.Text = ALFABETO[columna];
+                gridBotonera.Children.Add(boton, columna, fila);
+            }
+            
         }
     }
 }
