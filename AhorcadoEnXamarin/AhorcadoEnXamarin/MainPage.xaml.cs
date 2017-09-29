25131f4b89ab14385069a7c2a08616bf350dd4ed
tcrurav <tiburcio.cruz@gmail.com>
2017-09-29 19:05:32 +0100

    Día 29/09/2017

--- AhorcadoEnXamarin/AhorcadoEnXamarin/MainPage.xaml.cs
+++ AhorcadoEnXamarin/AhorcadoEnXamarin/MainPage.xaml.cs
@@ -9,7 +9,14 @@ namespace AhorcadoEnXamarin
 {
     public partial class MainPage : ContentPage
     {
-        readonly string[] ALFABETO = { "A", "B", "C", "D", "E", "F", "G"};
+        readonly string[] ALFABETO = { "A", "B", "C", "D", "E", "F", "G",
+                                        "A", "B", "C", "D", "E", "F", "G",
+                                        "A", "B", "C", "D", "E", "F", "G",
+                                        "A", "B", "C", "D", "E", "F", "*"};
+        int numFallos = 0;
+        readonly int MAX_FALLOS = 6;
+        Button[] letrasPulsadas;
+
         public MainPage()
         {
             InitializeComponent();
@@ -19,15 +26,50 @@ namespace AhorcadoEnXamarin
 
         private void Inicializar()
         {
-            imageAhorcado.Source = ImageSource.FromResource("AhorcadoEnXamarin.img.1.png");
-            int fila = 0;
-            for(int columna = 0; columna < 7; columna++)
+            letrasPulsadas = new Button[MAX_FALLOS];
+            imageAhorcado.Source = ImageSource.FromResource("AhorcadoEnXamarin.img.0.png");
+            for(int fila = 0; fila < 4; fila++)
             {
-                Button boton = new Button();
-                boton.Text = ALFABETO[columna];
-                gridBotonera.Children.Add(boton, columna, fila);
+                for (int columna = 0; columna < 7; columna++)
+                {
+                    Button boton = new Button
+                    {
+                        Text = ALFABETO[fila * 7 + columna]
+                    };
+                    boton.Clicked += Boton_Clicked;
+                    gridBotonera.Children.Add(boton, columna, fila);
+                }
+            }   
+        }
+
+        private void Boton_Clicked(object sender, EventArgs e)
+        {
+            if(((Button)sender).Text == "*")
+            {
+                Reiniciar();
+            }
+
+            if(numFallos < MAX_FALLOS)
+            {
+                numFallos++;
+                imageAhorcado.Source = ImageSource.FromResource("AhorcadoEnXamarin.img." + numFallos + ".png");
+
+                ((Button)sender).BackgroundColor = Color.Red;
+            } else
+            {
+                Reiniciar();
             }
             
+        }
+
+        private void Reiniciar()
+        {
+            numFallos = 0;
+            imageAhorcado.Source = ImageSource.FromResource("AhorcadoEnXamarin.img." + numFallos + ".png");
+            for(int i = 0; i < MAX_FALLOS; i++)
+            {
+                letrasPulsadas[i].BackgroundColor = Color.Default;
+            }
         }
     }
 }
